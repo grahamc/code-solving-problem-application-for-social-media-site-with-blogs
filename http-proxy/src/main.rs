d9aad16320c0ace49d2efa22e069ee746b41a520
Graham Christensen <graham@grahamc.com>
2016-06-15 22:43:40 -0500

    Move proxy commands

--- http-proxy/src/main.rs
+++ http-proxy/src/main.rs
@@ -5,112 +5,18 @@ extern crate regex;
 
 use std::net::{TcpListener, TcpStream};
 use std::thread;
-use std::io::{BufReader, Read, Write};
+use std::io::{BufReader, Write};
 // use std::str;
-pub use httpstate::{Header, HttpRequest, HttpResponse, HttpRequestParser, HttpResponseParser,
+pub use httpstate::{Header, HttpResponse, HttpRequestParser, HttpResponseParser,
                     HttpReader};
 pub use httperrors::HttpError;
-
+pub use proxy::do_proxy;
 mod httperrors;
 mod httplineparser;
 mod httpstate;
-
-fn backend_connect<'a>(backend: &'a str) -> Result<TcpStream,HttpError> {
-    match TcpStream::connect(backend) {
-        Ok(connection) => { Ok(connection) }
-        Err(e) => { Err(HttpError::gateway_timeout(e)) }
-    }
-}
-
-fn xfer_client_body_to_server<'a>(length: usize,
-                                  client: &'a mut BufReader<TcpStream>,
-                                  server: &'a mut TcpStream)
-                                  -> Result<(), HttpError> {
-    let mut remaining = 0;
-
-    while remaining < length {
-        let mut buf = [0; 4096];
-        match client.read(&mut buf) {
-            Ok(n) => {
-                remaining += n;
-            }
-            Err(e) => {
-                return Err(HttpError::client_timeout(e));
-            }
-        }
-
-        if let Err(e) = server.write(&buf) {
-            return Err(HttpError::gateway_timeout(e));
-        }
-    }
-
-    return Ok(());
-}
-
-fn xfer_server_body_to_client<'a>(length: usize,
-                                  server: &'a mut BufReader<TcpStream>,
-                                  client: &'a mut TcpStream)
-                                  -> Result<(), HttpError> {
-
-    let mut remaining = 0;
-
-    while remaining < length {
-        let mut buf = [0; 4096];
-        match server.read(&mut buf) {
-            Ok(n) => {
-                remaining += n;
-            }
-            Err(e) => {
-                return Err(HttpError::gateway_timeout(e))
-            }
-        }
-        if let Err(e) = client.write(&buf) {
-            return Err(HttpError::client_timeout(e));
-        }
-    }
-
-    return Ok(());
-}
+mod proxy;
 
 
-fn do_proxy<'a>(client_stream: &mut BufReader<TcpStream>,
-                backend: &'a str)
-                -> Result<(), HttpError> {
-
-    let mut parsed_request = try!(HttpRequestParser::new(client_stream).parse());
-
-    let client_ip = client_stream.get_ref().peer_addr().unwrap();
-    let forwarded_for = Header::from_string(format!("X-Forwarded-For: {}\r\n", client_ip));
-    parsed_request.add_header(forwarded_for.unwrap());
-
-    let mut server_stream = try!(backend_connect(backend));
-    if let Err(e) = server_stream.write(&parsed_request.as_string().as_bytes()) {
-        return Err(HttpError::gateway_timeout(e));
-    }
-
-    try!(xfer_client_body_to_server(parsed_request.body_length(),
-                                    client_stream,
-                                    &mut server_stream));
-
-
-    let mut server_stream = BufReader::new(server_stream);
-    let mut response = try!(HttpResponseParser::new(&mut server_stream).parse());
-
-    let backend = Header::from_string(format!("X-Backend: {}\r\n", &backend));
-    response.add_header(backend.unwrap());
-
-
-    let mut client_stream = client_stream.get_mut();
-    if let Err(e) = client_stream.write(&response.as_string().as_bytes()) {
-        println!("Encountered error sending headers to the client: {}", e);
-    }
-
-    try!(xfer_server_body_to_client(response.body_length(),
-                                    &mut server_stream,
-                                    &mut client_stream));
-
-    return Ok(());
-}
 
 fn return_error_to_client(mut client_stream: BufReader<TcpStream>, error: HttpError) {
     let response = HttpResponse::from_error(error);
---
+++ http-proxy/src/proxy.rs
@@ -0,0 +1,104 @@
+
+
+use std::io::{BufReader, Read, Write};
+use std::net::TcpStream;
+use httpstate::{Header, HttpRequestParser, HttpResponseParser};
+use httperrors::HttpError;
+
+
+pub fn do_proxy<'a>(client_stream: &mut BufReader<TcpStream>,
+                backend: &'a str)
+                -> Result<(), HttpError> {
+
+    let mut parsed_request = try!(HttpRequestParser::new(client_stream).parse());
+
+    let client_ip = client_stream.get_ref().peer_addr().unwrap();
+    let forwarded_for = Header::from_string(format!("X-Forwarded-For: {}\r\n", client_ip));
+    parsed_request.add_header(forwarded_for.unwrap());
+
+    let mut server_stream = try!(backend_connect(backend));
+    if let Err(e) = server_stream.write(&parsed_request.as_string().as_bytes()) {
+        return Err(HttpError::gateway_timeout(e));
+    }
+
+    try!(xfer_client_body_to_server(parsed_request.body_length(),
+                                    client_stream,
+                                    &mut server_stream));
+
+
+    let mut server_stream = BufReader::new(server_stream);
+    let mut response = try!(HttpResponseParser::new(&mut server_stream).parse());
+
+    let backend = Header::from_string(format!("X-Backend: {}\r\n", &backend));
+    response.add_header(backend.unwrap());
+
+
+    let mut client_stream = client_stream.get_mut();
+    if let Err(e) = client_stream.write(&response.as_string().as_bytes()) {
+        println!("Encountered error sending headers to the client: {}", e);
+    }
+
+    try!(xfer_server_body_to_client(response.body_length(),
+                                    &mut server_stream,
+                                    &mut client_stream));
+
+    return Ok(());
+}
+
+
+fn backend_connect<'a>(backend: &'a str) -> Result<TcpStream,HttpError> {
+    match TcpStream::connect(backend) {
+        Ok(connection) => { Ok(connection) }
+        Err(e) => { Err(HttpError::gateway_timeout(e)) }
+    }
+}
+
+fn xfer_client_body_to_server<'a>(length: usize,
+                                  client: &'a mut BufReader<TcpStream>,
+                                  server: &'a mut TcpStream)
+                                  -> Result<(), HttpError> {
+    let mut remaining = 0;
+
+    while remaining < length {
+        let mut buf = [0; 4096];
+        match client.read(&mut buf) {
+            Ok(n) => {
+                remaining += n;
+            }
+            Err(e) => {
+                return Err(HttpError::client_timeout(e));
+            }
+        }
+
+        if let Err(e) = server.write(&buf) {
+            return Err(HttpError::gateway_timeout(e));
+        }
+    }
+
+    return Ok(());
+}
+
+fn xfer_server_body_to_client<'a>(length: usize,
+                                  server: &'a mut BufReader<TcpStream>,
+                                  client: &'a mut TcpStream)
+                                  -> Result<(), HttpError> {
+
+    let mut remaining = 0;
+
+    while remaining < length {
+        let mut buf = [0; 4096];
+        match server.read(&mut buf) {
+            Ok(n) => {
+                remaining += n;
+            }
+            Err(e) => {
+                return Err(HttpError::gateway_timeout(e))
+            }
+        }
+        if let Err(e) = client.write(&buf) {
+            return Err(HttpError::client_timeout(e));
+        }
+    }
+
+    return Ok(());
+}
